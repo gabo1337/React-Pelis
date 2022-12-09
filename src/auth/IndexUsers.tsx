555f6a4d0b3f03cd0e376a3a8d72c29aacb286d3
Jose1060 <josean-1060@hotmail.com>
2022-12-09 12:21:16 -0500

    6.0

--- src/auth/IndexUsers.tsx
+++ src/auth/IndexUsers.tsx
@@ -1,67 +1,81 @@
-import IndexEntity from '../utils/IndexEntity';
-import { userDTO } from './auth.models';
-import { urlAccounts } from '../endpoints';
-import customConfirm from '../utils/customConfirm';
-import Button from '../utils/Button';
-import axios from 'axios';
-import Swal from 'sweetalert2';
+import IndexEntity from "../utils/IndexEntity";
+import { userDTO } from "./auth.models";
+import { urlAccounts } from "../endpoints";
+import customConfirm from "../utils/customConfirm";
+import Button from "../utils/Button";
+import axios from "axios";
+import Swal from "sweetalert2";
+import "./styles.css";
 
 export default function IndexUsers() {
+	async function makeAdmin(id: string) {
+		await doAdmin(`${urlAccounts}/makeAdmin`, id);
+	}
 
-    async function makeAdmin(id: string) {
-        await doAdmin(`${urlAccounts}/makeAdmin`, id);
-    }
+	async function removeAdmin(id: string) {
+		await doAdmin(`${urlAccounts}/removeAdmin`, id);
+	}
 
-    async function removeAdmin(id: string) {
-        await doAdmin(`${urlAccounts}/removeAdmin`, id);
-    }
+	async function doAdmin(url: string, id: string) {
+		await axios.post(url, JSON.stringify(id), {
+			headers: { "Content-Type": "application/json" },
+		});
 
-    async function doAdmin(url: string, id: string){
-        await axios.post(url, JSON.stringify(id), {
-            headers: {'Content-Type': 'application/json'}
-        });
+		Swal.fire({
+			title: "Success",
+			text: "Operation finished correctly",
+			icon: "success",
+		});
+	}
 
-        Swal.fire({
-            title: 'Success',
-            text: 'Operation finished correctly',
-            icon: 'success'
-        });
-    }
+	return (
+		<div className="users">
+			<div className="users_container">
+				<IndexEntity<userDTO> title="Users" url={`${urlAccounts}/listUsers`}>
+					{(users) => (
+						<>
+							<thead>
+								<tr>
+									<th></th>
+									<th>Email</th>
+								</tr>
+							</thead>
+							<tbody>
+								{users?.map((user) => (
+									<tr key={user.id}>
+										<td>{user.email}</td>
+										<td>
+											<Button
+												className="btn btn-primary waves-effect waves-light"
+												onClick={() =>
+													customConfirm(
+														() => makeAdmin(user.id),
+														`Quieres hacer a ${user.email} administrador?`,
+														"Guardar"
+													)
+												}>
+												Crear Admin
+											</Button>
 
-    return (
-        <IndexEntity<userDTO>
-            title="Users" url={`${urlAccounts}/listUsers`}
-        >
-            {users => <>
-                <table className="table table-dark table-striped table-nowrap">
-                <thead>
-                    <tr>
-                        <th></th>
-                        <th>Email</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {users?.map(user => <tr key={user.id}>
-                        <td>
-                            <Button className="btn btn-primary waves-effect waves-light"
-                                onClick={() => customConfirm(() => makeAdmin(user.id),
-                                    `Quieres hacer a ${user.email} administrador?`, 'Guardar')}
-                            >Crear Admin</Button>
-
-                            <Button
-                            className="btn btn-danger bg-gradient waves-effect waves-light ms-2"
-                                onClick={() => customConfirm(() => removeAdmin(user.id),
-                                    `Quieres eliminar a ${user.email} de administrador?`, 
-                                    'Guardar')}
-                            >Eliminar Admin</Button>
-                        </td>
-                        <td>
-                            {user.email}
-                        </td>
-                    </tr>)}
-                </tbody>
-                </table>
-            </>}
-        </IndexEntity>
-    )
+											<Button
+												className="btn btn-danger bg-gradient waves-effect waves-light ms-2"
+												onClick={() =>
+													customConfirm(
+														() => removeAdmin(user.id),
+														`Quieres eliminar a ${user.email} de administrador?`,
+														"Guardar"
+													)
+												}>
+												Eliminar Admin
+											</Button>
+										</td>
+									</tr>
+								))}
+							</tbody>
+						</>
+					)}
+				</IndexEntity>
+			</div>
+		</div>
+	);
 }
