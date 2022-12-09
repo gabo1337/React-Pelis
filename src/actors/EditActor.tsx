4c281cec8bea9013d82d68274b0efab48b1b86b7
Jose1060 <josean-1060@hotmail.com>
2022-12-09 12:45:04 -0500

    Edit

--- src/actors/EditActor.tsx
+++ src/actors/EditActor.tsx
@@ -1,32 +1,37 @@
-import { urlActors } from '../endpoints'
-import EditEntity from '../utils/EditEntity'
-import { convertActorToFormData } from '../utils/formDataUtils'
-import ActorForm from './ActorForm'
-import { actorCreationDTO, actorDTO } from './actors.model'
+import { urlActors } from "../endpoints";
+import EditEntity from "../utils/EditEntity";
+import { convertActorToFormData } from "../utils/formDataUtils";
+import ActorForm from "./ActorForm";
+import { actorCreationDTO, actorDTO } from "./actors.model";
+import "./styles.css";
 
-export default function EditActor(){
+export default function EditActor() {
+	function transform(actor: actorDTO): actorCreationDTO {
+		return {
+			name: actor.name,
+			pictureURL: actor.picture,
+			biography: actor.biography,
+			dateOfBirth: new Date(actor.dateOfBirth),
+		};
+	}
 
-    function transform(actor: actorDTO): actorCreationDTO {
-        return {
-            name: actor.name,
-            pictureURL: actor.picture,
-            biography: actor.biography,
-            dateOfBirth: new Date(actor.dateOfBirth)
-        }
-    }
-
-    return (
-        <EditEntity<actorCreationDTO, actorDTO> 
-         url={urlActors} indexURL="/actors" entityName="Actor" 
-         transformFormData={convertActorToFormData}
-         transform={transform}
-        >
-            {(entity, edit) => 
-                <ActorForm 
-                    model={entity}
-                    onSubmit={async values => await edit(values)}
-                />
-            }
-        </EditEntity>
-    )
+	return (
+		<div className="actor">
+			<div className="actor_container">
+				<EditEntity<actorCreationDTO, actorDTO>
+					url={urlActors}
+					indexURL="/actors"
+					entityName="Actor"
+					transformFormData={convertActorToFormData}
+					transform={transform}>
+					{(entity, edit) => (
+						<ActorForm
+							model={entity}
+							onSubmit={async (values) => await edit(values)}
+						/>
+					)}
+				</EditEntity>
+			</div>
+		</div>
+	);
 }
